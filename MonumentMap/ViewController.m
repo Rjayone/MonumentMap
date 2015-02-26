173987633ba093cf1a67a15b6ed417482614ec1b
Andrew Medvedev <admin@Mac-Admin.local>
2015-02-26 16:33:12 +0300

    Initial Commit

---
+++ MonumentMap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MonumentMap
+//
+//  Created by Andrew Medvedev on 26.02.15.
+//  Copyright (c) 2015 Andrew Medvedev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
